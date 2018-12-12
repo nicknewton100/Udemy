88aa98f8ed42966d86f36286bcc12bf62f013ba8
Nicholas Newton <nicholasnewton27@yahoo.com>
2018-12-12 14:39:54 -0500

    gkldfmng

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int max = 1000;
+        int min = 1;
+        Debug.Log("Welcome to number wizard , yo");
+        Debug.Log("Pick a number: ");
+        Debug.Log("The highest number is: " + max);
+        Debug.Log("The lowest number is: " + min);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
