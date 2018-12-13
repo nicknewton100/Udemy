2ffad040b124a8815118ec6211226879267607af
Nicholas Newton <nicholasnewton27@yahoo.com>
2018-12-13 09:24:48 -0500

    Text101 project started

---
+++ Text101/Text101/Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+    //allows this to be availible inspector
+    [SerializeField] Text textComponent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        textComponent.text = ("hi") ;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
