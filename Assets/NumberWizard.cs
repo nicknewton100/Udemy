b10146f670b6c772dd5915cfc3bd987f5841f86e
Nicholas Newton <nicholasnewton27@yahoo.com>
2018-12-13 01:35:35 -0500

    done

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        int max = 1000;
-        int min = 1;
+    int max;
+    int min;
+    int guess;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Welcome to number wizard , yo");
-        Debug.Log("Pick a number: ");
+        Debug.Log("Pick a number");
         Debug.Log("The highest number is: " + max);
         Debug.Log("The lowest number is: " + min);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        Debug.Log("Is the number higher or lower than " + guess);
+        Debug.Log("Push Up if it is Higher, Push Down if Lower, or Push Enter if its Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (max + min) / 2;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (max + min) / 2;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I am so smart!");
+            StartGame();
+        }
+    }
+    void NextGuess()
+    {
+        Debug.Log("Is it higher or lower than... " + guess);
+        Debug.Log(guess);
+    }
 }
